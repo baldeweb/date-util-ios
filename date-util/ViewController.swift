24964e994812ab3f03a77dea97aa5e89f30e808a
Wallace Baldenebre <wallacecorin@gmail.com>
2021-10-04 23:11:31 -0300

    first commit

--- date-util/ViewController.swift
+++ date-util/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let originalDate = "2021-10-04"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd"
+        let targetDate = dateFormatter.date(from: originalDate)?.onlyDate ?? Date()
+        
+        let today = Date().onlyDate
+        print("LOG >> HOJE: \(today)")
+        print("LOG >> DATA ALVO: \(targetDate)")
+        print(" ")
+        
+        if today > targetDate  {
+            print("LOG >> DEPOIS da data")
+        } else {
+            print("LOG >> ANTES ou IGUAL a data")
+        }
     }
-
-
 }
 
+extension Date {
+    var onlyDate: Date {
+        get {
+            let calender = Calendar.current
+            let dateComponents = calender.dateComponents([.year, .month, .day], from: self)
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy:MM:dd"
+            let date = calender.date(from: dateComponents)
+            let stringDate = dateFormatter.string(from: date!)
+            return dateFormatter.date(from: stringDate)!
+        }
+    }
+}
